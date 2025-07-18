75055fc628c49384b1d83c363f0cc0a97b6900ff
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-07-18 13:59:59 -0400

    dark mode works using props bc tailwind dark: doesnt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import HomePage from './pages/HomePage.tsx'
 import Header from './components/Header.tsx'
 import Footer from './components/Footer.tsx'
@@ -10,12 +11,29 @@ export default function App() {
   const Layout = () => {
     return (
       <div>
-        <Header/>
-        <Outlet/>
+        <Header onToggleTheme={toggleTheme} darkMode={darkMode} />
+        <Outlet context={{ darkMode }}/>
         <Footer/>
       </div>  
     )
   }
+
+  // light mode
+  const [darkMode, setDarkMode] = useState<boolean>(true)
+
+  const toggleTheme = () => {
+      setDarkMode(prev => !prev) // recommended over set(!dark)
+  }
+
+  useEffect(()=> {
+      if (darkMode) {
+          document.documentElement.style.setProperty('--background-color', '#18181B');
+          document.body.style.backgroundColor = '#18181B';
+      } else {
+          document.documentElement.style.setProperty('--background-color', '#ffffff');
+          document.body.style.backgroundColor = '#ffffff';
+      }
+  }, [darkMode])
 
   return (
     <div>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,9 @@
-export default function Header(){
-    
-    const handleUIChange = () => {
-        console.log("Changing ui");
-    }
+type HeaderProps = {
+  onToggleTheme: () => void
+  darkMode: boolean
+}
+
+export default function Header({ onToggleTheme, darkMode: darkMode} : HeaderProps){
 
     const scrollToSpot = (pixels: number): void => {
         window.scrollTo({ top: pixels, behavior: 'smooth'});
@@ -15,25 +16,29 @@ export default function Header(){
     return (
         // TODO small screen header reorg
         // TODO if on other page, redirect + scroll --> probably pass a hash
-        <div className="flex flex-row w-full justify-between h-12 mt-2 px-2 bg-purple-900">
+        <div className={`flex flex-row w-full justify-between h-12 px-2 ${darkMode ? `bg-purple-800` : `bg-purple-700`}`}>
             {/* UI Change button */}
             <div className="flex w-30 justify-center">
-                <button onClick={handleUIChange} className=" transition-all cursor-pointer hover:text-zinc-900 font-medium duration-200 self-center px-2 py-1 bg-purple-600 rounded-lg text-white hover:bg-purple-700 active:bg-purple-700">Light mode</button>
+                <button onClick={onToggleTheme} className={`transition-all cursor-pointer font-medium duration-200 self-center px-2 py-1 bg-purple-600 rounded-lg active:bg-purple-900 ${
+                    darkMode
+                    ? 'text-white hover:text-zinc-900 hover:bg-purple-700' 
+                    : 'text-zinc-900 hover:text-white hover:bg-purple-800'
+                    }`}>{darkMode ? "Light mode" : "Dark mode"}</button>
             </div>
 
             {/* Middle */}
             <div className="flex flex-row items-center text-zinc-900">
-                <div className="w-18 text-center"><a onClick={() => scrollToSpot(700)} className="inline-block mx-2 cursor-pointer text-zinc-900 font-bold text-lg transition-all hover:text-white duration-300 hover:scale-125">Games</a></div>
-                <div className="w-18 text-center"><a onClick={() => scrollToSpot(950)} className="inline-block mx-2 cursor-pointer text-zinc-900 font-bold text-lg transition-all hover:text-white duration-300 hover:scale-125">Team</a></div>
-                <div className="w-18 text-center"><a onClick={openJobsPage} className="inline-block mx-2 cursor-pointer text-zinc-900 font-bold text-lg transition-all hover:text-white duration-300 hover:scale-125">Jobs</a></div>
+                <div className="w-18 text-center"><a onClick={() => scrollToSpot(700)} className={`inline-block mx-2 cursor-pointer font-bold text-lg transition-all  duration-300 hover:scale-125 ${darkMode ? `text-white hover:text-zinc-900` : `text-zinc-900 hover:text-white`}`}>Games</a></div>
+                <div className="w-18 text-center"><a onClick={() => scrollToSpot(950)} className={`inline-block mx-2 cursor-pointer font-bold text-lg transition-all duration-300 hover:scale-125 ${darkMode ? `text-white hover:text-zinc-900` : `text-zinc-900 hover:text-white`}`}>Team</a></div>
+                <div className="w-18 text-center"><a onClick={openJobsPage} className={`inline-block mx-2 cursor-pointer font-bold text-lg transition-all duration-300 hover:scale-125 ${darkMode ? `text-white hover:text-zinc-900` : `text-zinc-900 hover:text-white`}`}>Jobs</a></div>
                 
                 
             </div>
             
             {/* Icons */}
             <div className="flex justify-center w-30">
-                <a className="self-center cursor-pointer" href="mailto:tbspgames@gmail.com"><img src="mail_icon.png" className="hover:invert h-9 object-contain transition-all duration-300 hover:scale-125 mr-2" ></img></a>
-                <a className="self-center cursor-pointer" href="https://tbspgames.itch.io/"><img src="itch-io-seeklogo.png" className="hover:invert h-6 object-contain transition-all duration-300 hover:scale-125" ></img></a>
+                <a className="self-center cursor-pointer" href="mailto:tbspgames@gmail.com"><img src="mail_icon.png" className={`h-9 object-contain transition-all duration-300 hover:scale-125 mr-2 ${darkMode ? `invert hover:invert-0` : `invert-0 hover:invert`}`} ></img></a>
+                <a className="self-center cursor-pointer" href="https://tbspgames.itch.io/"><img src="itch-io-seeklogo.png" className={`h-6 object-contain transition-all duration-300 hover:scale-125 ${darkMode ? `invert hover:invert-0` : `invert-0 hover:invert`}`} ></img></a>
             </div>
         </div>
     )
