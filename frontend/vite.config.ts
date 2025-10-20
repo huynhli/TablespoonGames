95cafd7a729fdb5e17d7e4d5e5cd98cfedf2e7a8
huynhli <liam.huynh@mail.utoronto.ca>
2025-10-20 17:45:02 -0400

    docker compose + dependency fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import VitePluginSitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
